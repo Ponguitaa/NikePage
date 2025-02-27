58716f78abf0eeea74f28ca7b5fc2b4bcadea7e5
Sergi Ponga Cano <sergiponcan@campus.monlau.com>
2025-02-27 19:35:30 +0100

    a

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,5 @@
 import { Routes } from '@angular/router';
+import { HeaderComponent } from './components/header/header.component';
+import { LandingPageComponent } from "./components/landing-page/landing-page.component";
 
 export const routes: Routes = [];
